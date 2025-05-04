88b2d189c8ba9bd2b0eb9a1a4d22049bc4e3d095
Akira Maetomo <akiramaetomo@users.noreply.github.com>
2025-05-04 13:40:21 +0900

    for analyse

---
+++ assets/audio/wavcp.bat
@@ -0,0 +1,12 @@
+copy white-lin.wav move.wav
+copy sweep100-800.wav rotate.wav
+copy ping-3k.wav land.wav
+copy sweep4k-2k.wav fix.wav
+copy sin6k_4wav.wav clear.wav
+rem copy sqr-35.wav drop.wav
+copy drop_swp2.wav drop.wav
+copy loop2_v3.wav bgm_play.wav
+copy loop1_v3.wav bgm_init.wav
+copy gameover3.wav bgm_over.wav
+copy explosion_2.00ms.wav se_over.wav
+
---
+++ assets/js/input/keyboard.js
@@ -0,0 +1,41 @@
+///keyboard.js
+//リピート無視
+import { ACTIONS, InputHandler } from './inputHandler.js';
+
+export class KeyboardInputHandler extends InputHandler {
+    constructor() {
+        super();
+        this.keymap = {
+            a: ACTIONS.MOVE_LEFT,
+            s: ACTIONS.MOVE_RIGHT,
+            z: ACTIONS.SOFT_DROP,
+            Enter: ACTIONS.ENTER,          //game start
+            ArrowLeft: ACTIONS.ROTATE_L,
+            ArrowRight: ACTIONS.ROTATE_R,
+            ArrowUp: ACTIONS.MOVE_UP,
+            ArrowDown: ACTIONS.MOVE_DOWN,
+            ' ': ACTIONS.ENTER,
+            Space: ACTIONS.ENTER,
+            r: ACTIONS.RESTART,
+            R: ACTIONS.RESTART,
+            c: ACTIONS.BACK,
+            C: ACTIONS.BACK,
+            m: ACTIONS.BACK,
+            M: ACTIONS.BACK
+        };
+        window.addEventListener('keydown', e => {
+            if (e.repeat) return;                 // ★ repeat を丸ごと無視
+            console.log('[DBG] keydown', e.key);   //for debug
+            const act = this.keymap[e.key];
+            if (act) this.active.add(act);
+        });
+        window.addEventListener('keyup', e => {
+            const act = this.keymap[e.key];
+            if (act) this.active.delete(act);
+        });
+    }
+
+    poll() {
+        // Poll handled in main loop. No internal _afterPoll here.
+    }
+}
